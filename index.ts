dbf2ded053a1dc02dd65822ab5633e8a0d3cd65a
FlameWolf <agnijith@gmail.com>
2022-07-11 15:48:17 +0530

    Add sample code for `fastify-multipart`

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@
 
 import fastify, { FastifyInstance, FastifyRequest, FastifyReply, FastifyPluginOptions, HookHandlerDoneFunction } from "fastify";
 import multer, { memoryStorage } from "fastify-multer";
+import fastifyMultipart from "@fastify/multipart";
 import { File } from "fastify-multer/lib/interfaces";
 import fastifySwagger from "@fastify/swagger";
 
@@ -9,6 +10,21 @@ const isProdEnv = process.env.NODE_ENV === "production";
 if (!isProdEnv) {
 	require("dotenv").config();
 }
+const postCreateSchema = {
+	consumes: ["multipart/form-data"],
+	body: {
+		type: "object",
+		properties: {
+			content: {
+				type: "string"
+			},
+			media: {
+				type: "string",
+				format: "binary"
+			}
+		}
+	}
+};
 const uploadMediaFile = multer({
 	limits: {
 		fileSize: 1024 * 1024 * 5
@@ -16,39 +32,26 @@ const uploadMediaFile = multer({
 	storage: memoryStorage()
 }).single("media");
 const server: FastifyInstance = fastify({ logger: true });
-server.register(multer.contentParser).after(() => {
-	if (!isProdEnv) {
-		server.register(fastifySwagger, {
-			routePrefix: "/swagger",
-			exposeRoute: true,
-			openapi: {
-				info: {
-					title: "Fastify Upload",
-					version: "1.0.0"
-				}
+if (!isProdEnv) {
+	server.register(fastifySwagger, {
+		routePrefix: "/swagger",
+		exposeRoute: true,
+		openapi: {
+			info: {
+				title: "Fastify Upload",
+				version: "1.0.0"
 			}
-		});
-	}
+		}
+	});
+}
+/* CODE FOR FASTIFY-MULTER */
+server.register(multer.contentParser).after(() => {
 	server.register(
 		(instance: FastifyInstance, options: FastifyPluginOptions, next: HookHandlerDoneFunction) => {
 			instance.post(
 				"/create",
 				{
-					schema: {
-						consumes: ["multipart/form-data"],
-						body: {
-							type: "object",
-							properties: {
-								content: {
-									type: "string"
-								},
-								media: {
-									type: "string",
-									format: "binary"
-								}
-							}
-						}
-					},
+					schema: postCreateSchema,
 					preHandler: uploadMediaFile
 				},
 				(request: FastifyRequest, reply: FastifyReply) => {
@@ -68,6 +71,30 @@ server.register(multer.contentParser).after(() => {
 		{ prefix: "/posts" }
 	);
 });
+/* CODE FOR FASTIFY-MULTIPART WHICH ALSO GIVES THE SAME ERROR  */
+/* COMMENT OUT THE FASTIFY-MULTER BLOCK BEFORE UNCOMMENTING THE BELOW LINES */
+// server.register(fastifyMultipart).after(() => {
+// 	server.register(
+// 		(instance: FastifyInstance, options: FastifyPluginOptions, next: HookHandlerDoneFunction) => {
+// 			instance.post(
+// 				"/create",
+// 				{
+// 					schema: postCreateSchema
+// 				},
+// 				async (request: FastifyRequest, reply: FastifyReply) => {
+// 					const content = (request.body as any).content as string;
+// 					const file = await request.file();
+// 					reply.send({
+// 						content,
+// 						file: JSON.stringify(file) || "No file selected"
+// 					});
+// 				}
+// 			);
+// 			next();
+// 		},
+// 		{ prefix: "/posts" }
+// 	);
+// });
 server.setErrorHandler((err: Error, request: FastifyRequest, reply: FastifyReply) => {
 	request.log.error(err.toString());
 	console.error(err.stack);
