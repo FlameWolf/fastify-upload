64290510105d4dbdfcc12c873f534d1c0ee59470
FlameWolf <agnijith@gmail.com>
2022-07-26 11:43:27 +0530

    Refactor code

--- form-data-parser.ts
+++ form-data-parser.ts
@@ -1,67 +1,65 @@
 "use strict";
 
-import { FastifyPluginAsync, FastifyPluginOptions, FastifySchema } from "fastify";
+import { FastifyPluginAsync, FastifyPluginOptions } from "fastify";
 import busboy = require("busboy");
-import { Busboy, Limits, FileInfo } from "busboy";
+import { Limits, FileInfo } from "busboy";
+import { Readable } from "stream";
 
 interface Dictionary extends Object {
 	[key: string | symbol]: any;
 }
-interface FormDataParserPluginOptions extends Limits, FastifyPluginOptions {}
-type FormDataParserPlugin = FastifyPluginAsync<FormDataParserPluginOptions> & Dictionary;
-class File {
+export interface FormDataParserPluginOptions extends Limits, FastifyPluginOptions {}
+export type FormDataParserPlugin = FastifyPluginAsync<FormDataParserPluginOptions> & Dictionary;
+export class FileField {
 	fieldName?: string;
 	fileName!: string;
 	encoding!: string;
 	mimeType!: string;
 	data!: Buffer;
 
-	constructor(fileInfo?: FileInfo) {
-		if (fileInfo) {
-			this.fileName = fileInfo.filename;
-			this.encoding = fileInfo.encoding;
-			this.mimeType = fileInfo.mimeType;
+	constructor(name?: string, info?: FileInfo) {
+		this.fieldName = name;
+		if (info) {
+			this.fileName = info.filename;
+			this.encoding = info.encoding;
+			this.mimeType = info.mimeType;
 		}
 	}
 }
 declare module "fastify" {
 	interface FastifyRequest {
-		__files__?: Array<File>;
+		__files__?: Array<FileField>;
 	}
 }
 
 const formDataParser: FormDataParserPlugin = async (instance, options) => {
 	instance.addContentTypeParser("multipart/form-data", (request, message, done) => {
-		const fileList: Array<File> = [];
-		const formData: Dictionary = {};
-		const schemaProps = (request.context as Dictionary).schema?.body?.properties;
+		const files: Array<FileField> = [];
+		const body: Dictionary = {};
+		const props = (request.context as Dictionary).schema?.body?.properties;
 		const bus = busboy({ headers: message.headers, limits: options });
-		bus.on("file", (fieldName: string, file: Busboy, fileInfo: FileInfo) => {
-			const chunks: Array<Uint8Array> = [];
-			const fileObject = new File(fileInfo);
-			fileObject.fieldName = fieldName;
-			file.on("data", data => chunks.push(data));
-			file.on("close", () => {
-				fileObject.data = Buffer.concat(chunks);
-				fileList.push(fileObject);
-				formData[fieldName] = JSON.stringify(fileInfo);
+		bus.on("file", (name: string, stream: Readable, info: FileInfo) => {
+			const data: Array<Uint8Array> = [];
+			const file = new FileField(name, info);
+			stream.on("data", chunk => data.push(chunk));
+			stream.on("close", () => {
+				file.data = Buffer.concat(data);
+				files.push(file);
+				body[name] = JSON.stringify(info);
 			});
 		});
-		bus.on("field", (fieldName, fieldValue) => {
-			if (schemaProps) {
-				const schemaType = schemaProps[fieldName]?.type;
-				if (schemaType !== "string") {
-					try {
-						formData[fieldName] = JSON.parse(fieldValue);
-						return;
-					} catch (err) {}
-				}
+		bus.on("field", (name, value) => {
+			if (props && props[name]?.type !== "string") {
+				try {
+					body[name] = JSON.parse(value);
+					return;
+				} catch (err) {}
 			}
-			formData[fieldName] = fieldValue;
+			body[name] = value;
 		});
 		bus.on("close", () => {
-			request.__files__ = fileList;
-			done(null, formData);
+			request.__files__ = files;
+			done(null, body);
 		});
 		bus.on("error", (error: Error) => {
 			done(error);
@@ -69,13 +67,13 @@ const formDataParser: FormDataParserPlugin = async (instance, options) => {
 		message.pipe(bus);
 	});
 	instance.addHook("preHandler", async (request, reply) => {
-		const requestBody = request.body as Dictionary;
-		const requestFiles = request.__files__ as Array<File>;
-		if (requestFiles?.length) {
-			for (const fileObject of requestFiles) {
+		const body = request.body as Dictionary;
+		const files = request.__files__ as Array<FileField>;
+		if (files?.length) {
+			for (const fileObject of files) {
 				const fieldName = fileObject.fieldName as string;
 				delete fileObject.fieldName;
-				requestBody[fieldName] = fileObject;
+				body[fieldName] = fileObject;
 			}
 		}
 		delete request.__files__;
