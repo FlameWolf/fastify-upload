42465dc91b75af0223e7f3256656c6d02aa7ddef
FlameWolf <agnijith@gmail.com>
2022-07-26 12:55:19 +0530

    Refactor code

--- form-data-parser.ts
+++ form-data-parser.ts
@@ -9,7 +9,7 @@ interface Dictionary extends Object {
 	[key: string | symbol]: any;
 }
 export interface FormDataParserPluginOptions extends Limits, FastifyPluginOptions {}
-export interface IFile {
+export interface File {
 	field?: string;
 	name: string;
 	encoding: string;
@@ -17,7 +17,7 @@ export interface IFile {
 	data: Buffer;
 }
 export type FormDataParserPlugin = FastifyPluginAsync<FormDataParserPluginOptions> & Dictionary;
-class File implements IFile {
+class FileInternal implements File {
 	field?: string;
 	name!: string;
 	encoding!: string;
@@ -34,19 +34,19 @@ class File implements IFile {
 }
 declare module "fastify" {
 	interface FastifyRequest {
-		__files__?: Array<IFile>;
+		__files__?: Array<File>;
 	}
 }
 
 const formDataParser: FormDataParserPlugin = async (instance, options) => {
 	instance.addContentTypeParser("multipart/form-data", (request, message, done) => {
-		const files: Array<IFile> = [];
+		const files: Array<File> = [];
 		const body: Dictionary = {};
 		const props = (request.context as Dictionary).schema?.body?.properties;
 		const bus = busboy({ headers: message.headers, limits: options });
 		bus.on("file", (name: string, stream: Readable, info: FileInfo) => {
 			const data: Array<Uint8Array> = [];
-			const file = new File(name, info);
+			const file = new FileInternal(name, info);
 			stream.on("data", chunk => data.push(chunk));
 			stream.on("close", () => {
 				file.data = Buffer.concat(data);
@@ -74,7 +74,7 @@ const formDataParser: FormDataParserPlugin = async (instance, options) => {
 	});
 	instance.addHook("preHandler", async (request, reply) => {
 		const body = request.body as Dictionary;
-		const files = request.__files__ as Array<IFile>;
+		const files = request.__files__ as Array<File>;
 		if (files?.length) {
 			for (const fileObject of files) {
 				const field = fileObject.field as string;
