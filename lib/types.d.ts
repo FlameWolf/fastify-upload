765f1cefe10f99e9f4cc3d7c138973319b47e52f
FlameWolf <agnijith@gmail.com>
2022-07-31 10:50:30 +0530

    Create types.d.ts

---
+++ lib/types.d.ts
@@ -0,0 +1,35 @@
+"use strict";
+
+import { FastifyPluginAsync, FastifyPluginOptions } from "fastify";
+import { Limits, FileInfo } from "busboy";
+import { Readable } from "stream";
+
+export interface Dictionary extends Object {
+	[key: string | symbol]: any;
+}
+export interface StorageOption {
+	process: (name: string, stream: Readable, info: FileInfo) => File;
+}
+export interface FileSaveTarget {
+	directory?: string;
+	fileName?: string;
+}
+export interface FormDataParserPluginOptions extends FastifyPluginOptions {
+	limits?: Limits;
+	storage?: StorageOption;
+}
+export interface File {
+	field: string | undefined;
+	originalName: string;
+	encoding: string;
+	mimeType: string;
+	path: string | undefined;
+	stream: Readable | undefined;
+	data: Buffer | undefined;
+}
+export type FormDataParserPlugin = FastifyPluginAsync<FormDataParserPluginOptions> & Dictionary;
+export declare module "fastify" {
+	interface FastifyRequest {
+		__files__?: Array<File>;
+	}
+}
